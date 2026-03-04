1346530a7a764c831eafc648cb2ae416057d3d6e
Jeremy Wu <58065981+jeremywuworldwidesimtan@users.noreply.github.com>
2026-03-04 17:20:14 +0800

    switch to using HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { HashRouter , Route, Routes } from "react-router";
 import Layout from "./pages/layout.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        <Route path="/bluepages-2026/" element={<Layout />}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<App />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
